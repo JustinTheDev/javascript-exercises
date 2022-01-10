ccca727ee001cc2bcd29f3f955567ca9ecf3504b
Justin <Justin.TheDev@gmail.com>
2022-01-10 13:34:04 -0600

    Completed challenge 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(submitString, numOfTimes) {
+    let returnString = "";
+    for(let i =0; i<numOfTimes; i++)
+    {
+        returnString += submitString;
+    }
+    if(numOfTimes < 0)
+    {
+        return "ERROR";
+    }
+    return returnString;
 };
 
 // Do not edit below this line
