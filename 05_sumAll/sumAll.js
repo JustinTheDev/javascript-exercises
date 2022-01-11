0de7e41e86694d5a50002be40c398d471969f8dd
Justin <Justin.TheDev@gmail.com>
2022-01-10 20:44:39 -0600

    completed challenge 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum=0;
+    if (typeof num1 === 'string' || typeof num2 === 'string')
+    {
+        return "ERROR";
+    }
+    else if (num1 < num2 && num1 > 0)
+    {
+        for(let i = num1; i<=num2; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+    else if(num2 < num1 && num2 >0)
+    {
+        for(let i = num2; i<=num1; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
