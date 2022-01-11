20ae7e1b83e25aee881e7183548c89648fd3496a
Justin <Justin.TheDev@gmail.com>
2022-01-10 20:01:47 -0600

    Completed challenge 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    let removeItems = Array.from(items);
 
+    for (let i = 0; i<removeItems.length; i++)
+    {
+        let index = arr.indexOf(removeItems[i]);
+        if(index !== -1)
+        {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
