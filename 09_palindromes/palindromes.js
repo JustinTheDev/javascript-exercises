21d6031d68b97f2230020cdd85e5daa83f2751cb
Justin <Justin.TheDev@gmail.com>
2022-01-15 17:10:08 -0600

    Finished the challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~/\s/g';
+const palindromes = function (str) {
+    let lowerString = str.toLowerCase();
+    lowerString = lowerString.replace(/\s/g, '');
+    let splitStr = lowerString.split("");
+    noPunctuationArr = splitStr.filter(function(letter){
+        return punctuation.indexOf(letter) === -1;
+    });
+    let originalNoPunc = noPunctuationArr.join("");
+    let reverseArr = noPunctuationArr.reverse();
+    joinArr = reverseArr.join("");
+    if (originalNoPunc === joinArr)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
