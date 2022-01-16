35decafb56041340379ac4246f2d46931b3942dd
Justin <Justin.TheDev@gmail.com>
2022-01-15 18:12:15 -0600

    Completed the challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const goldenRatio = 1.618034;
+const fibonacci = function(inputNum) {
+    let num = Number(inputNum);
+    if (num < 0)
+    {
+        return "OOPS";
+    }
+    let fib = [];
+    fib[0] = 1;
+    fib[1] = 1;
+    for(let i = 2; i< num; i++)
+    {
+        fib[i] = fib[i-2]+fib[i-1];
+    }
+    return fib.at(-1);
 };
 
 // Do not edit below this line
