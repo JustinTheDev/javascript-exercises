934417c9655f869425e36909e691ce08c33b4715
Justin <Justin.TheDev@gmail.com>
2022-01-17 13:37:00 -0600

    completed the challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a,b)
+    {
+        const currentYear = (new Date()).getFullYear();
+        if(a.yearOfDeath == null)
+        {
+            a.yearOfDeath = currentYear;
+        }
+        else if(b.yearOfDeath == null)
+        {
+            b.yearOfDeath = currentYear;
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1:1;
+    });
+    console.log(people[0]);
+    return people[0];
 
 };
 
